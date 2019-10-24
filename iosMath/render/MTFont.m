643a70df3af72055367e5698cc4d4b9b75a81279
Luke Zhao <me@lkzhao.com>
2019-10-24 09:44:05 -0700

    load from own bunble

--- iosMath/render/MTFont.m
+++ iosMath/render/MTFont.m
@@ -70,7 +70,7 @@
 + (NSBundle*) fontBundle
 {
     // Uses bundle for class so that this can be access by the unit tests.
-    return [NSBundle bundleWithURL:[[NSBundle bundleForClass:[self class]] URLForResource:@"mathFonts" withExtension:@"bundle"]];
+    return [NSBundle bundleForClass:[self class]];
 }
 
 - (MTFont *)copyFontWithSize:(CGFloat)size
